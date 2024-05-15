61b9c9b0c17163ac6e9014d7e99b4883eef058bd
badrex <badr.nlp@gmail.com>
2024-05-15 20:52:35 +0200

    add topic only embeddings

--- embedding_generation/generate_embeddings.py
+++ embedding_generation/generate_embeddings.py
@@ -15,6 +15,7 @@ from typing import List, Dict
 # parse command line arguments 
 model_id = sys.argv[1]
 output_file = sys.argv[2]
+add_topics = sys.argv[3]
 
 # read JSON files from disk
 json_files: List = [
@@ -36,6 +37,7 @@ for doc in tqdm(json_files):
 print(f"Num of JSON objects: {len(doc2json)}")
 
 
+
 documents: List[str] = [
     doc2json[doc]['tldr'] for doc in doc2json
 ]
@@ -45,6 +47,18 @@ article_identifiers: List[str] = [
     doc2json[doc]['article_id'] for doc in doc2json
 ]
 
+topics_sentences: List[str] = [
+    'This article discusses the following topics: ' +
+    ', '.join(doc2json[doc]['topics'][:-1]) + 
+    ', and ' + doc2json[doc]['topics'][-1] + '.'
+    for doc in doc2json
+]
+
+documents_plus_topics: List[str] = [
+    ' '.join((doc, topics)) for doc, topics in zip(documents, topics_sentences)
+]
+
+print(documents_plus_topics[-1])
 
 # This will trigger the model download and initialization
 #model_id = 'BAAI/bge-small-en-v1.5'
@@ -52,7 +66,14 @@ embedding_model = TextEmbedding(model_id)
 print(f"The model {model_id} is ready to use.")
 
 # reminder this is a generator
-embeddings_generator = embedding_model.embed(documents)  
+if add_topics == 'with_topics':
+    embeddings_generator = embedding_model.embed(documents_plus_topics)
+
+elif add_topics == 'only_topics':
+    embeddings_generator = embedding_model.embed(topics_sentences)  
+
+else:
+    embeddings_generator = embedding_model.embed(documents)  
 
 embeddings: List[str] = [] 
 
